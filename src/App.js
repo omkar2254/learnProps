3f63235bec818e40910e627a4ae79581c9dd1b11
Omkar <omkarpatil2254@gmail.com>
2024-01-30 10:29:39 +0530

    Basic Props

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Card from './Components/Card'
 function App() {
+  let myObj = {
+    name: "Omkar",
+    role: "Software Developer"
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1 className='bg-green-400 mx-3 mb-4'>Tailwind</h1>
+    <Card channel="chaiAurCode" someObj={myObj}/>
+    <Card/>
+    </>
   );
 }
 
